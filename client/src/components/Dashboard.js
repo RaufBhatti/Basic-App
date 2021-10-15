f8b92252c066d32247df43f764f46086d7049656
RaufBhatti <76686853+RaufBhatti@users.noreply.github.com>
2021-10-15 17:35:33 +0500

    Updating Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ const Dashboard = () => {
     return (
         <div className='container text-center mt-5'>
             <h2>Dashboard</h2>
+            <p>That's the main page</p>
         </div>
     );
 }
